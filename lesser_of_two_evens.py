1c7ca53def7e71ca03fa780c84ca650efa7f688a
Johnson P Thomas <46226683+johnsonpthomas@users.noreply.github.com>
2019-01-07 17:10:53 -0600

    Create lesser_of_two_evens.py

---
+++ lesser_of_two_evens.py
@@ -0,0 +1,9 @@
+def lesser_of_two_evens(a,b):
+    if a < b and a % 2 ==0 and b % 2==0:
+        print(a)
+    else:
+        print(b)
+
+lesser_of_two_evens(2,4)
+lesser_of_two_evens(2,5)
+lesser_of_two_evens(1,3)
