b3f755a3aa11c1d9947ea63aca7e41717a5a9a6a
Johnson P Thomas <46226683+johnsonpthomas@users.noreply.github.com>
2019-01-22 11:19:55 -0600

    Update pallindrome.py

--- pallindrome.py
+++ pallindrome.py
@@ -1,6 +1,7 @@
 def pallindrome(a):
 	#print(a)
 	#print(a[::-1])
+	#-- Check if the string is same as the reverse of the string using ::-1
 	if a == a[::-1]:
 	    print(a + ' is pallindrome')
 	else:
